f2e3f391d6c99d8d1999d6c389d2179a11d93f57
Karo <sylvain.brunet37@gmail.com>
2026-03-08 19:15:53 -0400

    refactor: moved login logic in PlayerController

--- UnrealClient/Source/UnrealClient/ClientPlayerController.cpp
+++ UnrealClient/Source/UnrealClient/ClientPlayerController.cpp
@@ -1,12 +1,170 @@
 ﻿#include "ClientPlayerController.h"
 
+#include "BackendSubsystem.h"
+#include "SerializationUtils.h"
+#include "UserData.h"
+#include "UserSession.h"
+#include "GenericPlatform/GenericPlatformHttp.h"
+#include "Kismet/GameplayStatics.h"
+
 void AClientPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
+	SetInputModeToUIOnly();
+	
+	BeginPlayAsync();
+}
+
+void AClientPlayerController::SetInputModeToUIOnly()
+{
 	bShowMouseCursor = true;
 	FInputModeUIOnly InputMode;
 	InputMode.SetWidgetToFocus(nullptr);
 	InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	SetInputMode(InputMode);
+}
+
+UE5Coro::TCoroutine<bool> AClientPlayerController::LogAndCreateSession()
+{
+	if (not co_await LoginAndSaveUser())
+	{
+		co_return false;
+	}
+
+	if (not co_await CreateAndSaveSession())
+	{
+		co_return false;
+	}
+
+	co_return true;
+}
+
+UE5Coro::TCoroutine<bool> AClientPlayerController::RetrieveUserData()
+{
+	UserData = SerializationUtils::DeserializeUserData(UserDataSlotName, UserIndex);
+	check(UserData);
+
+	UE_LOG(LogTemp, Log, TEXT("Loaded UserData from save."));
+
+	if (UGameplayStatics::DoesSaveGameExist(SessionSlotName, UserIndex))
+	{
+		UserSession = SerializationUtils::DeserializeUserSession(SessionSlotName, UserIndex);
+		check(UserSession);
+
+		UE_LOG(LogTemp, Log, TEXT("Loaded UserSession from save."));
+
+		if (not co_await RefreshAndSaveSession())
+		{
+			co_return false;
+		}
+	}
+	else
+	{
+		if (not co_await CreateAndSaveSession())
+		{
+			co_return false;
+		}
+	}
+
+	co_return true;
+}
+
+UE5Coro::TCoroutine<> AClientPlayerController::BeginPlayAsync()
+{
+	const bool bSuccessfullyLoggedIn =
+		UGameplayStatics::DoesSaveGameExist(UserDataSlotName, UserIndex)
+			? co_await RetrieveUserData()
+			: co_await LogAndCreateSession();
+	
+	if (not bSuccessfullyLoggedIn)
+	{
+		co_return;
+	}
+}
+
+UE5Coro::TCoroutine<bool> AClientPlayerController::LoginAndSaveUser()
+{
+	const UBackendSubsystem* BackendSubsystem = GetGameInstance()->GetSubsystem<UBackendSubsystem>();
+	check(BackendSubsystem);
+
+	const FHttpRequestRef Request = BackendSubsystem->CreateSimpleHttpRequest(
+		"Post",
+		"users"
+	);
+
+	const auto Response =
+		co_await BackendSubsystem->MakeHttpRequest(Request);
+
+	if (not Response)
+	{
+		UE_LOG(LogTemp, Error, TEXT("LoginAndSaveUser: Failed to connect to backend"));
+		co_return false;
+	}
+
+	UserData = SerializationUtils::DeserializeUserData(*Response);
+	check(UserData);
+
+	SerializationUtils::SaveUserData(UserData, UserDataSlotName, UserIndex);
+	UE_LOG(LogTemp, Log, TEXT("LoginAndSaveUser Complete"));
+
+	co_return true;
+}
+
+UE5Coro::TCoroutine<bool> AClientPlayerController::CreateAndSaveSession()
+{
+	const UBackendSubsystem* BackendSubsystem = GetGameInstance()->GetSubsystem<UBackendSubsystem>();
+	check(BackendSubsystem);
+
+	const FHttpRequestRef Request =
+		BackendSubsystem->CreateSimpleHttpRequest(
+			"Post",
+			"users/" + UserData->UserId + "/sessions/create",
+			FString::Printf(TEXT("guestToken=%s"), *FGenericPlatformHttp::UrlEncode(UserData->GuestToken))
+		);
+
+	const auto Response =
+		co_await BackendSubsystem->MakeHttpRequest(Request);
+
+	if (not Response)
+	{
+		UE_LOG(LogTemp, Error, TEXT("CreateAndSaveSession: Failed to connect to backend"));
+		co_return false;
+	}
+
+	UserSession = SerializationUtils::DeserializeUserSession(*Response);
+	check(UserSession);
+
+	SerializationUtils::SaveUserSession(UserSession, SessionSlotName, UserIndex);
+	UE_LOG(LogTemp, Log, TEXT("CreateAndSaveSession Complete"));
+
+	co_return true;
+}
+
+UE5Coro::TCoroutine<bool> AClientPlayerController::RefreshAndSaveSession()
+{
+	const UBackendSubsystem* BackendSubsystem = GetGameInstance()->GetSubsystem<UBackendSubsystem>();
+	check(BackendSubsystem);
+
+	const FString AuthHeader = FString::Printf(TEXT("Bearer %s"), *UserSession->SessionToken);
+	const FHttpRequestRef Request = BackendSubsystem->CreateSimpleHttpRequest(
+		"Post", "users/" + UserData->UserId + "/sessions/refresh");
+	Request->SetHeader("Authorization", AuthHeader);
+
+	const auto Response =
+		co_await BackendSubsystem->MakeHttpRequest(Request);
+
+	if (not Response)
+	{
+		UE_LOG(LogTemp, Error, TEXT("RefreshAndSaveSession: Failed to connect to backend"));
+		co_return false;
+	}
+
+	UserSession = SerializationUtils::DeserializeUserSession(*Response);
+	check(UserSession);
+
+	SerializationUtils::SaveUserSession(UserSession, SessionSlotName, UserIndex);
+	UE_LOG(LogTemp, Log, TEXT("RefreshAndSaveSession Complete"));
+
+	co_return true;
 }
--- UnrealClient/Source/UnrealClient/ClientPlayerController.h
+++ UnrealClient/Source/UnrealClient/ClientPlayerController.h
@@ -2,8 +2,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "UE5Coro.h"
 
 #include "ClientPlayerController.generated.h"
+
+class UUserSession;
+class UUserData;
 
 UCLASS()
 class UNREALCLIENT_API AClientPlayerController : public APlayerController
@@ -11,5 +15,32 @@ class UNREALCLIENT_API AClientPlayerController : public APlayerController
 	GENERATED_BODY()
 
 protected:
+	UPROPERTY(BlueprintReadWrite)
+	UUserData* UserData;
+
+	UPROPERTY(BlueprintReadWrite)
+	UUserSession* UserSession;
+
+	UPROPERTY(BlueprintReadonly, EditAnywhere)
+	FString UserDataSlotName = "UserDataSlot";
+
+	UPROPERTY(BlueprintReadonly, EditAnywhere)
+	FString SessionSlotName = "SessionSlot";
+
+	UPROPERTY(BlueprintReadonly, EditAnywhere)
+	int32 UserIndex = 0;
+
 	virtual void BeginPlay() override;
+
+private:
+	void SetInputModeToUIOnly();
+	
+	UE5Coro::TCoroutine<> BeginPlayAsync();
+
+	[[nodiscard]] UE5Coro::TCoroutine<bool> LogAndCreateSession();
+	[[nodiscard]] UE5Coro::TCoroutine<bool> RetrieveUserData();
+
+	[[nodiscard]] UE5Coro::TCoroutine<bool> LoginAndSaveUser();
+	[[nodiscard]] UE5Coro::TCoroutine<bool> CreateAndSaveSession();
+	[[nodiscard]] UE5Coro::TCoroutine<bool> RefreshAndSaveSession();
 };
